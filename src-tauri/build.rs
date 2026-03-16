6d3c9239e165922d8650272598b6e1273a74a74d
Brad McAllister <brad@realcode.co.uk>
2026-03-16 15:42:18 +0000

    fix: resolve missing npcap-sdk in github actions

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    let npcap_lib_x64 = std::env::current_dir().unwrap().join("npcap-sdk").join("Lib").join("x64");
+    if npcap_lib_x64.exists() {
+        println!("cargo:rustc-link-search=native={}", npcap_lib_x64.display());
+    }
     tauri_build::build()
 }
